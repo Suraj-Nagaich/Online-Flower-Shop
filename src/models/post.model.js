370c6fbecc6915057a917229c15e313703289c17
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-09-10 21:17:28 +0530

    initial commit

---
+++ src/models/post.model.js
@@ -0,0 +1,14 @@
+
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  Seller: String,
+  category: String,
+  content: String,
+  image: String,
+  email: String,     
+  date: Date
+});
+
+module.exports = mongoose.model("Post", postSchema);
